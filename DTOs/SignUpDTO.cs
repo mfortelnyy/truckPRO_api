b5d4a0a78b259fe411cff8855a6a394ee25fb646
mfortelnyy <78120259+mfortelnyy@users.noreply.github.com>
2024-09-06 16:15:32 -0500

    add compare data annotation
    Ensures the confirmation password and password are the same

--- DTOs/SignUpDTO.cs
+++ DTOs/SignUpDTO.cs
@@ -9,8 +9,11 @@ namespace truckPRO_api.DTOs
         [Required]
         public string Username { get; set; }
         [Required]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
         [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = " The password and confirmation password do not match")]
         public string ConfirmPassword { get; set; }
         [Required]
         public string FirstName { get; set; }
