ca34d8b54cb58793f46a03753eeaa443182deb55
mfortelnyy <78120259+mfortelnyy@users.noreply.github.com>
2024-09-13 16:38:28 -0500

    Add new Email Service
    Will be used to communicate with users

---
+++ Services/EmailService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
+
+namespace truckPRO_api.Services
+{
+    public class EmailService : IEmailService
+    {
+        Task IEmailService.SendEmailAsync(string email, string subject, string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
