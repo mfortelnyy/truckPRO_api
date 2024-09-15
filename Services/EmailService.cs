f7ae882cd0f80043cfc36b4fdd12c5cc14e4de93
mfortelnyy <78120259+mfortelnyy@users.noreply.github.com>
2024-09-15 16:49:56 -0500

    Add fluentEmail package
    Delete mailKit
    FluentEmail will be used instead to send verification code to confirm email

--- Services/EmailService.cs
+++ Services/EmailService.cs
@@ -7,9 +7,38 @@ namespace truckPRO_api.Services
 {
     public class EmailService : IEmailService
     {
-        Task IEmailService.SendEmailAsync(string email, string subject, string message)
+        public async Task SendEmailAsync(string email, string subject, string message)
         {
-            throw new NotImplementedException();
+            
+            
+            // Create a new SMTP client to send the email
+            using (var client = new SmtpClient())
+            {
+                try
+                {
+                    // Connect to the SMTP server (replace with your actual settings)
+                    await client.ConnectAsync("smtp.gmail.com", 587, SecureSocketOptions.StartTls);
+
+                    // Authenticate with the SMTP server (replace with your actual email/password)
+                    await client.AuthenticateAsync("fc.molochko@gmail.com", "Max11072001!");
+                    
+                 
+                    // Send the email
+                    await client.SendAsync(mm);
+                }
+                catch (Exception ex)
+                {
+                    // Handle any exceptions
+                    Console.WriteLine($"Error sending email: {ex.Message}");
+                    //throw ex;  
+                }
+                finally
+                {
+                    // Disconnect the client and clean up
+                    await client.DisconnectAsync(true);
+                    client.Dispose();
+                }
+            }
         }
     }
 }
