c4278083f4a26df14ec6ff646429e795fbe512b9
mfortelnyy <78120259+mfortelnyy@users.noreply.github.com>
2024-09-16 17:18:45 -0500

    Add Manager controller
    All manager endpoints will be routed through this controller now

---
+++ Controllers/ManagerController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using truckPRO_api.Services;
+
+namespace truckPRO_api.Controllers
+{
+    public class ManagerController(IManagerService managerService): Controller
+    {
+        [HttpGet]
+        [Route("/adm/addDriverByCompanyId")]
+        [Authorize(Roles = "Manager")]
+        public async Task<IActionResult> AddDriverByCompanyId(int companyId)
+        {
+            var res = await managerService.AddDriverByCompanyId(companyId);
+            return Ok(res);
+        }
+    }
+}
