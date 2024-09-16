429a7ce1bc5cba16ff3aa87ba1809b4d60a0d7fc
mfortelnyy <78120259+mfortelnyy@users.noreply.github.com>
2024-09-15 19:32:37 -0500

    Perform migration
    Database will store an email verification token to account for email verification process and a flag to indicate if email is verified or not

---
+++ Migrations/20240916002954_AddEmailVerification.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace truckPRO_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEmailVerification : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "EmailVerificationToken",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "EmailVerified",
+                table: "User",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EmailVerificationToken",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "EmailVerified",
+                table: "User");
+        }
+    }
+}
