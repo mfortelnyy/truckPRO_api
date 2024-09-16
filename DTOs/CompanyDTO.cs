a422ebfb6ff2b1d41698240883ed11292b15b3e0
mfortelnyy <78120259+mfortelnyy@users.noreply.github.com>
2024-09-15 23:07:06 -0500

    Add attributes to company DTO

--- DTOs/CompanyDTO.cs
+++ DTOs/CompanyDTO.cs
@@ -1,6 +1,15 @@
-﻿namespace truckPRO_api.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+using truckPRO_api.Models;
+
+namespace truckPRO_api.DTOs
 {
     public class CompanyDTO
     {
+
+        [Required]
+        [MinLength(2), MaxLength(50)]
+        public string Name { get; set; }
+
+        //public List<User> users { get; set; }   
     }
 }
