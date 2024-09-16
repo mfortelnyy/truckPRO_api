de949a64390b7be86773842d565c82168617d72a
mfortelnyy <78120259+mfortelnyy@users.noreply.github.com>
2024-09-16 17:17:30 -0500

    Add new model
    PendingUser table will support the functionality of storing pending driver users with only emails.
    Model includes fields: Id, Email, InvitationSent, CreatedDate, CompanyId, and InvitationToken.
    Implemented basic validation and constraints for the new model.

---
+++ Models/PendingUser.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace truckPRO_api.Models
+{
+    [Table("PendingUser")]
+    public class PendingUser
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public DateTime CreatedDate { get; set; }
+        [Required]
+        [ForeignKey("CompanyId")]
+        public int CompanyId { get; set; }
+        public bool InvitationSent { get; set; }
+
+    }
+}
