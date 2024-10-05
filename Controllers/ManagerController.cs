1d0dd3a78b6b61248113eedfd64416be23a5ff77
mfortelnyy <mfortelnyy@hawk.iit.edu>
2024-10-05 13:01:11 -0500

    Add two endpoints for manager

--- Controllers/ManagerController.cs
+++ Controllers/ManagerController.cs
@@ -272,5 +272,61 @@ namespace truckPRO_api.Controllers
 
 
 
+        [HttpGet]
+        [Authorize(Roles = "Manager")]
+        [Route("allDriversByCompany")]
+        public async Task<IActionResult> GetAllDriversByCompany()
+        {
+            try
+            {
+                var token = Request.Headers.Authorization.ToString().Replace("Bearer ", "");
+                string? cIdString = User.FindFirst("companyId").Value;
+
+                Console.WriteLine("companyid:  "+cIdString);
+
+                if (cIdString != null) 
+                {
+                    var companyId = int.Parse(cIdString);
+                    var drivers = await managerService.GetAllDriversByCompany(companyId);
+                    if (drivers == null || drivers.Count == 0)
+                    {
+                        return NotFound("Sorry, no drivers found in your company!");
+                    }
+                    return Ok(drivers);
+                }
+                return BadRequest("company id nor found from token!");
+                
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [Authorize(Roles = "Manager")]
+        [Route("allLogsByDriver")]
+        public async Task<IActionResult> GetAllLogsByDriver(int driverId)
+        {
+            try
+            {
+                var companyId = int.Parse(User.FindFirst("companyId").Value);
+                var allLogs = await managerService.GetLogsByDriver(driverId, companyId);
+                if (allLogs == null || allLogs.Count == 00)
+                {
+                    return NotFound("Sorry, driver has no logs at this moment!");
+                }
+                return Ok(allLogs);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    
+
+
+
+
     }
 }
