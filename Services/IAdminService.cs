f17274d0563a602f0522d91bff8fb8b11a774d64
mfortelnyy <mfortelnyy@hawk.iit.edu>
2024-10-11 00:55:12 -0500

    Add delete function for admin service interface

--- Services/IAdminService.cs
+++ Services/IAdminService.cs
@@ -7,14 +7,14 @@ namespace truckPRO_api.Services
     {
         public Task<List<Company>> GetAllComapnies();
         public Task<List<User>> GetAllDrivers();
-
         public Task<List<User>> GetDriversByComapnyId(int id);
         public Task<User> GetDriverById(int id);
         public Task<List<LogEntry>> GetLogsByDriverId(int id);
         public Task<bool> CreateCompany(CompanyDTO companyDTO);
-
         public Task<List<User>> GetAllManagers();
         public Task<bool> DeleteCompany(int companyId);
+        public Task<bool> DeleteManager(int userId);
+
        
     }
 }
