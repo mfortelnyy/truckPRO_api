5f73e911df10a9e50dee0ad8d3815a41f793d498
mfortelnyy <mfortelnyy@hawk.iit.edu>
2024-10-22 22:05:50 -0500

    Add Fluent Api constraint for user table

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -11,6 +11,14 @@ namespace truckPRO_api.Data
         public DbSet<Company> Company { get; set; }
         public DbSet<LogEntry> LogEntry { get; set; }
         public DbSet<PendingUser> PendingUser { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .HasIndex(b => b.Email)
+                .IsUnique();
+        }
     }
     
 }
