46fd7bc2723742aa7ffae0542de03d43d6cb7894
mfortelnyy <mfortelnyy@hawk.iit.edu>
2024-10-24 00:15:24 -0500

    Change method signature to return bool

--- Services/IEmailService.cs
+++ Services/IEmailService.cs
@@ -2,6 +2,6 @@
 {
     public interface IEmailService
     {
-        Task SendEmailAsync(string email, string subject, string message);
+        Task<bool> SendEmailAsync(string email, string subject, string message);
     }
 }
