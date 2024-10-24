fe29ca94383e1da0b7c143fa731a2721aed8684f
mfortelnyy <mfortelnyy@hawk.iit.edu>
2024-10-24 14:31:12 -0500

    Change signature of the method in interface

--- Services/IUserService.cs
+++ Services/IUserService.cs
@@ -7,7 +7,7 @@ namespace truckPRO_api.Services
     {
         Task<string> CreateUserAsync(SignUpDTO sDTO);
         Task<string> LoginUserAsync(LoginDTO lDTO);
-        Task<string> VerifyEmail(string emailToken);
+        Task<string> VerifyEmail(int userId, string emailToken);
         Task<string> UpdatePassword(int driverId, string oldPassword, string newPassword);
         Task<string> ForgetPassword(String email);
         Task<UserDTO> GetUserById(int userId);
