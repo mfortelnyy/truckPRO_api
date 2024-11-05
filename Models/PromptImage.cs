4e0046b017194379da166d7f878377e87bc8cfaa
mfortelnyy <78120259+mfortelnyy@users.noreply.github.com>
2024-11-05 01:12:47 -0600

    Update PromptImage.cs

--- Models/PromptImage.cs
+++ Models/PromptImage.cs
@@ -8,7 +8,7 @@ namespace truckapi.Models
     public class PromptImage
     {
         //CI/CD pipeline test-18
-        //CI/CD pipeline Mac-6     
+        //CI/CD pipeline Mac-7     
         
         public string path { get; set; } // Original file path
         public int promptIndex { get; set; } // The prompt index associated with the image
