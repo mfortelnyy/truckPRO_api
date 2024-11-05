9b6e5488033918bf22a50c13c2c02d55b12298ba
mfortelnyy <mfortelnyy@hawk.iit.edu>
2024-11-04 18:56:50 -0600

    Test CI/CD pipeline from MacOS

--- Models/PromptImage.cs
+++ Models/PromptImage.cs
@@ -8,6 +8,7 @@ namespace truckapi.Models
     public class PromptImage
     {
         //CI/CD pipeline test-2s
+        //CI/CD pipeline mac test
         public string path { get; set; } // Original file path
         public int promptIndex { get; set; } // The prompt index associated with the image
 
