b1e72f234f92d8c61df700c2f62aa809e28f4795
mfortelnyy <78120259+mfortelnyy@users.noreply.github.com>
2024-11-17 04:02:56 -0600

    Implement user validation logic in a service
    ISP

---
+++ Services/IUserValidationService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using truckPRO_api.DTOs;
+
+namespace truckPRO_api.Services
+{
+    public interface IUserValidationService
+    {
+        public (bool isValid, string errorMessage) Validate(SignUpDTO dto);
+    }
+
+}
---
+++ Services/UserValidationService.cs
@@ -0,0 +1,23 @@
+﻿using truckPRO_api.DTOs;
+using truckPRO_api.Models;
+
+namespace truckPRO_api.Services
+{
+    public class UserValidationService :IUserValidationService
+    {
+        public (bool isValid, string errorMessage) Validate(SignUpDTO dto)
+        {
+            if (string.IsNullOrEmpty(dto.Password) || string.IsNullOrEmpty(dto.ConfirmPassword))
+                return (false, "Password fields cannot be empty.");
+
+            if (dto.Password != dto.ConfirmPassword)
+                return (false, "Passwords do not match.");
+
+            var role = (UserRole)dto.Role;
+            if ((role == UserRole.Driver || role == UserRole.Manager) && !dto.CompanyId.HasValue)
+                return (false, "CompanyId is required for drivers and managers.");
+
+            return (true, string.Empty);
+        }
+    }
+}
