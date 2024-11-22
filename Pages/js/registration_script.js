ce75cf30736eab3ca8c01d246dec971bece3e5f1
mfortelnyy <mfortelnyy@hawk.iit.edu>
2024-11-21 22:47:54 -0600

    Improve aimation for form submission
    CSS: set spinner by default hidden

--- Pages/js/registration_script.js
+++ Pages/js/registration_script.js
@@ -3,10 +3,14 @@ document.querySelector("form").addEventListener("submit", function (event) {
     var confirmPassword = document.getElementById("ConfirmPassword").value;
 
     if (password !== confirmPassword) {
+        //prevents submission if passwords do not match
         event.preventDefault();
         alert("Passwords do not match. Please try again.");
     } else {
+        //show spinner before submitting the form
         document.getElementById("spinner").style.display = "block";
+
+        //add delay to simulate submission
         setTimeout(function() {
             event.target.submit();
         }, 2000);
