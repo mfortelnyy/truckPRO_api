085fe987d3483f17017830c8126a06c2b01db87c
mfortelnyy <mfortelnyy@hawk.iit.edu>
2024-11-23 03:07:42 -0600

    Perform migrations of the DB to add Device Token field

---
+++ Migrations/20241123084959_AddDeviceTokenForNotifications.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace truckPRO_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDeviceTokenForNotifications : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FcmDeviceToken",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FcmDeviceToken",
+                table: "User");
+        }
+    }
+}
