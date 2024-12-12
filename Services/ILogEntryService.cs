236409a851746987cfd5fcae9b238375672c8ea8
mfortelnyy <mfortelnyy@hawk.iit.edu>
2024-12-11 20:42:25 -0600

    Change method signature in the interface
    Return LogEntryDto which can be either On Duty or Off Dutty containing a list of child logs

--- Services/ILogEntryService.cs
+++ Services/ILogEntryService.cs
@@ -1,4 +1,5 @@
-﻿using truckPRO_api.DTOs;
+﻿using truckPro_api.DTOs;
+using truckPRO_api.DTOs;
 using truckPRO_api.Models;
 
 namespace truckPRO_api.Services
@@ -11,11 +12,11 @@ namespace truckPRO_api.Services
         public Task<string> StopDrivingLog(int userId);
         public Task<string> StopOnDutyLog(int userId);
         public Task<string> StopOffDutyLog(int userId);
-        public Task<List<LogEntry>> GetActiveLogEntries(int driverId);
+        public Task<LogEntryParent> GetActiveLogEntries(int driverId);
         public Task<TimeSpan> GetTotalDrivingHoursLastWeek(int userId);
-        public Task<TimeSpan> GetTotalOnDutyHoursLastWeek(int userId);
+        public Task<TimeSpan> GetTotalOnDutyHoursLastWeek7days(int userId);
         public Task<TimeSpan> GetTotalOffDutyHoursLastWeek(int userId);
-        public Task<List<LogEntry>> GetAllLogs(int driverId);
+        public Task<List<LogEntryParent>> GetAllLogs(int driverId);
         public Task<bool> NotifyManagers(int userId);
 
     }
