d0c6f58db2a8d79b95619efb73845c52915ad762
mfortelnyy <mfortelnyy@hawk.iit.edu>
2025-01-23 23:51:10 -0600

    Create NotFound.cshtml.cs

---
+++ Pages/NotFound.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace truckPRO_api.Pages
+{
+    public class NotFoundModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
